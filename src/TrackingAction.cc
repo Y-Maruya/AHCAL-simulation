ee630d4a9285a81320dede03a6473dbd34102bc7
ymaruya <maruya@hep.phys.titech.ac.jp>
2025-01-23 13:16:04 +0900

    tracking criteria

--- src/TrackingAction.cc
+++ src/TrackingAction.cc
@@ -9,8 +9,16 @@ namespace SimCalModule
 
     void TrackingAction::PreUserTrackingAction(const G4Track* track) {
         fpTrackingManager->SetTrajectory(new G4Trajectory(track));
-        fpTrackingManager->SetStoreTrajectory(true);
-        if (track->GetVolume()->GetLogicalVolume()->GetName() 
+        // fpTrackingManager->SetStoreTrajectory(true);
+        if ((track->GetVolume()->GetLogicalVolume()->GetName() == "TrenchAndWallLog" || track->GetVolume()->GetLogicalVolume()->GetName() == "World_Logical")
+        && (track->GetNextVolume()->GetLogicalVolume()->GetName() == "TrenchAndWallLog" || track->GetNextVolume()->GetLogicalVolume()->GetName() == "World_Logical"))
+        {
+            if(track->GetParentID() != 0){
+                track->SetStoreTrajectory(false);
+            }
+        }    
+        else
+            track->SetStoreTrajectory(true);
     }
 
     void TrackingAction::PostUserTrackingAction(const G4Track* track) {

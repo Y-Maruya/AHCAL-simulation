e24c30f03d0207166b148ae999797d35bd123706
ymaruya <maruya@hep.phys.titech.ac.jp>
2025-01-24 13:33:08 +0900

    event abort for neutral hadron

--- src/TrackingAction.cc
+++ src/TrackingAction.cc
@@ -10,16 +10,28 @@ namespace SimCalModule
     void TrackingAction::PreUserTrackingAction(const G4Track* track) {
         fpTrackingManager->SetTrajectory(new G4Trajectory(track));
         // fpTrackingManager->SetStoreTrajectory(true);
-        if ((track->GetVolume()->GetLogicalVolume()->GetName() == "TrenchAndWallLog" || track->GetVolume()->GetLogicalVolume()->GetName() == "World_Logical")
-        && (track->GetNextVolume()->GetLogicalVolume()->GetName() == "TrenchAndWallLog" || track->GetNextVolume()->GetLogicalVolume()->GetName() == "World_Logical"))
+        // if ((track->GetVolume()->GetLogicalVolume()->GetName() == "TrenchAndWallLog" || track->GetVolume()->GetLogicalVolume()->GetName() == "World_Logical")
+        // && (track->GetNextVolume()->GetLogicalVolume()->GetName() == "TrenchAndWallLog" || track->GetNextVolume()->GetLogicalVolume()->GetName() == "World_Logical"))
+        // {
+        //     if(track->GetParentID() > 10){
+        //         fpTrackingManager->SetStoreTrajectory(false);
+        //     }
+        //     fpTrackingManager->SetStoreTrajectory(true);
+        // }    
+        // else
+        //     fpTrackingManager->SetStoreTrajectory(true);
+        if (track->GetParentID() == 0 && track->GetTrackID() == 1)
         {
-            if(track->GetParentID() > 10){
-                fpTrackingManager->SetStoreTrajectory(false);
+            if(track->GetVolume()->GetLogicalVolume()->GetName() == "World_Logical")
+            {
+                fpTrackingManager->EventAborted();
             }
             fpTrackingManager->SetStoreTrajectory(true);
-        }    
-        else
+        }else if (track->GetParentID() == 1){
             fpTrackingManager->SetStoreTrajectory(true);
+        }else{
+            fpTrackingManager->SetStoreTrajectory(false);
+        }
     }
     void TrackingAction::PostUserTrackingAction(const G4Track* track) {
     }
